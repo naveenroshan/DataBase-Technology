719394399f0dfec151460c6408c612f1b49b03cf
Naveen S <naveen.s2@happiestminds.com>
2021-11-04 11:25:04 +0530

    joins

--- Mysql/JOINS_Statments.sql
+++ Mysql/JOINS_Statments.sql
@@ -178,36 +178,17 @@ SELECT * FROM
 (SELECT 
  e.emp_no,
  e.first_name,
+ e.last_name,
+ NULL AS dept_no,
+ NULL AS from_date
+ FROM employees e
+ WHERE last_name = 'Denis'
+ UNION SELECT
+ NULL AS emp_no,
+ NULL AS first_name,
+ NULL AS last_name,
+ dm.dept_no,
+ dm.from_date
+ FROM dept_manager dm) as a
+ ORDER BY -a.emp_no DESC;
  
-
-
-
-
-
-drop table if exists employees_dup;
-CREATE TABLE employees_dup(
-emp_no int(11),
-birth_date date,
-first_name varchar(14),
-last_name varchar(16),
-gender enum('M', 'F'),
-hire_date date
-);
-
-INSERT INTO employees_dup
-SELECT e.* 
-FROM employees e
-LIMIT 20;
-
-INSERT INTO employees_dup VALUES
-(
-'10001',
-'1953-09-02',
-'Georgi',
-'Facello',
-'M',
-'1986-06-26'
-);
-
-select * from employees_dup
-where first_name = 'Georgi';
